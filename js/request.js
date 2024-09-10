b922f04f25c1804ad86e9593b75e7b127f1eb5cb
sergiobf18 <sergiobf@gmail.com>
2024-09-10 06:51:43 -0500

    request

--- js/request.js
+++ js/request.js
@@ -1,11 +1,25 @@
 //const url = 'http://localhost:5000/api'
-const url = 'https://backend142-o98d.onrender.com/api/'
+const url = 'https://backend142-o98d.onrender.com/api/';
 
-function sendRequest(endPoint, method, data){
-    let request = new XMLHttpRequest();
-    request.open(method, url + endPoint);
-    request.responseType = 'json';
-    request.setRequestHeader('Content-Type', 'application/json');
-    request.send(data ? JSON.stringify(data): data);
-    return request;
+async function sendRequest(endPoint, method, data) {
+    try {
+        const response = await fetch(url + endPoint, {
+            method: method,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: data ? JSON.stringify(data) : null
+        });
+
+        // Verificar si la respuesta fue exitosa
+        if (!response.ok) {
+            throw new Error('Error en la respuesta de la red');
+        }
+
+        // Retornar la respuesta en formato JSON
+        return await response.json();
+    } catch (error) {
+        console.error('Error en la solicitud:', error);
+        throw error;
+    }
 }
